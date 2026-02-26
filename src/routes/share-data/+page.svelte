ccbbcad9bbc3346d738a7101e1d714d49f7bd1f4
xivk <ben@anyways.eu>
2026-02-26 14:36:25 +0100

    Mark Strava integration as temporarily unavailable

--- src/routes/share-data/+page.svelte
+++ src/routes/share-data/+page.svelte
@@ -76,7 +76,7 @@
             <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
 
                 <!-- Strava -->
-                <div class="border border-gray-200 rounded-xl p-8 flex flex-col gap-6">
+                <div class="border border-gray-200 rounded-xl p-8 flex flex-col gap-6 opacity-60">
                     <div class="flex items-center gap-4">
                         <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <rect width="40" height="40" rx="8" fill="#FC4C02"/>
@@ -84,15 +84,16 @@
                             <path d="M16.5 14L13 21.2h3.5l3-6.2-3-6.8L13 21.2" fill="white" opacity="0.6"/>
                             <path d="M16.5 8.2l-3.5 13h3.5L19.5 14l-3-5.8z" fill="white"/>
                         </svg>
-                        <h3 class="text-xl font-bold text-gray-800 m-0">Strava</h3>
+                        <div>
+                            <h3 class="text-xl font-bold text-gray-800 m-0">Strava</h3>
+                            <span class="text-xs font-semibold text-red-500 uppercase tracking-wide">Temporarily unavailable</span>
+                        </div>
                     </div>
                     <p class="text-gray-600 text-sm flex-1">
-                        Connect your Strava account to automatically contribute your cycling activities to the Bike Data Project. Your past rides will also be synchronized.
+                        Strava is currently not giving us access and limits our integration to a single user. If you know of a workaround, please <a href="mailto:info@bikedataproject.org" class="text-[#FC4C02] underline">let us know</a>!
                     </p>
-                    <button
-                        onclick={linkStrava}
-                        class="w-full py-3 px-4 bg-[#FC4C02] text-white font-semibold rounded-lg hover:bg-[#e04402] transition-colors cursor-pointer">
-                        Link your Strava account
+                    <button disabled class="w-full py-3 px-4 bg-[#FC4C02] text-white font-semibold rounded-lg opacity-40 cursor-not-allowed">
+                        Currently unavailable
                     </button>
                 </div>
 
