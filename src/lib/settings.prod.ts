34a6b6b8ad0574b4413d2fa2695c05429a176f2d
xivk <ben@anyways.eu>
2026-02-26 13:37:35 +0100

    Added production settings and copy step in deploy workflow.

---
+++ src/lib/settings.prod.ts
@@ -0,0 +1,10 @@
+const settings = {
+    public_url: "https://www.bikedataproject.org",
+    auth: {
+        authority: "https://www.bikedataproject.org/api/users/realms/bdp",
+        client_id: "frontend",
+        scope: "openid profile email",
+    }
+};
+
+export { settings };
